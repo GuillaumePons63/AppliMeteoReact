45cb8221e695a87fcf01efb23a47faf0b08f1dcf
Guillaumeweb <guillaume@web-63.fr>
2022-07-23 13:44:32 +0200

    Menu

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 const app = document.getElementById('app');
 
-console.log(app);
+const Title = ({ }) => {
+    return <h1 className='title u-center'>
+        Mon Application Météo
+    </h1>
+}
+
+const Search = ({ }) => {
+    return <div>
+        <form className="mx-20 form-group">
+            <input type='text' className="form-group-input input--lg" placeholder="Entrez le nom d'une ville" />
+            <input type='submit' className="form-group-btn btn-primary btn--lg" value='rechercher'></input>
+        </form>
+    </div>
+}
+
+ReactDOM.render(
+    <React.Fragment>
+        <Title />
+        <Search />
+    </React.Fragment>,
+    app
+)
