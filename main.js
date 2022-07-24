649bea155a697680e0626034000b0a7cedfa29ee
Guillaumeweb <guillaume@web-63.fr>
2022-07-24 15:06:54 +0200

    ajout heure et date

--- main.js
+++ main.js
@@ -15,9 +15,36 @@ const Search = ({ }) => {
     </div>
 }
 
+class DateDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: null,
+            heure: null
+        }
+    }
+
+    componentDidMount() {
+        window.setInterval(() => {
+            const date = new Date();
+            this.setState(() => ({ date: date.toLocaleDateString(), heure: date.toLocaleTimeString('it-IT') }));
+        }, 1000)
+    }
+
+
+    render() {
+        return <div className='u-center'>
+            <p> Nous sommes le {(this.state.date)} et il est {(this.state.heure)}</p>
+        </div>
+
+    }
+}
+
+
 ReactDOM.render(
     <React.Fragment>
         <Title />
+        <DateDisplay />
         <Search />
     </React.Fragment>,
     app
